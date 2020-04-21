af110692fef379591db418cd49543b6d8761db54
ibrahim velinov <ibsukru@gmail.com>
2020-04-21 02:06:57 +0200

    dist

--- packages/mono.web/pages/index.tsx
+++ packages/mono.web/pages/index.tsx
@@ -1,6 +1,11 @@
+import { add as dist } from "@mono/lib/dist/src";
 import { add } from "@mono/lib";
 import { say } from "cowsay-browser";
+import { Fragment } from "react";
 
 export default () => (
-  <pre>{say({ text: `1 + 2 = ${add(1)(2).toString()}` })}</pre>
+  <Fragment>
+    <pre>{say({ text: `1 + 2 = ${add(1)(2).toString()}` })}</pre>
+    <pre>{say({ text: `2 + 1 = ${dist(2)(1).toString()}` })}</pre>
+  </Fragment>
 );
