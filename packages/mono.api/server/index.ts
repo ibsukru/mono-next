0be6b71694107656be412a7c99664946d20c4fc4
ibrahim velinov <ibsukru@gmail.com>
2020-04-22 16:41:21 +0200

    clear logs

--- packages/mono.api/server/index.ts
+++ packages/mono.api/server/index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { add } from "mono.lib";
-import partial from "mono.lib/src/add";
 import face from "mono.fun/random/faces";
 
 const port = process.env.PORT || "4000";
@@ -9,10 +7,7 @@ const server = express();
 server.listen(port, (err: unknown) => {
   if (err) throw err;
 
-  console.log(`face`, face());
-  console.info(`
-  API Ready on http://localhost:${port}
-    ${add(2)(3)}
-    ${partial(2)(3)}
+  console.info(`${face()} >
+    API Ready on http://localhost:${port}
   `);
 });
--- packages/mono.web/server/index.ts
+++ packages/mono.web/server/index.ts
@@ -1,16 +1,6 @@
 import express from "express";
 import next from "next";
-import { install } from "source-map-support";
-import face from "mono.fun/random/faces";
-import { add as src } from "mono.lib/src";
-import { add } from "mono.lib";
 import { serverAndClient } from "../common";
-install();
-
-console.info(serverAndClient(), {
-  src: src(1)(2),
-  add: add(1)(2),
-});
 
 const port = process.env.PORT || "3000";
 const dev = process.env.NODE_ENV !== "production";
@@ -26,6 +16,6 @@ app.prepare().then(() => {
 
   server.listen(port, (err) => {
     if (err) throw err;
-    console.info(`${face()}> Ready on http://localhost:${port}`);
+    console.info(`${serverAndClient()}> Ready on http://localhost:${port}`);
   });
 });
