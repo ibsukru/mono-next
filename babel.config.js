65319b2afd36751c10622b922e1cdb07835fce20
ibrahim velinov <ibsukru@gmail.com>
2020-04-22 16:04:31 +0200

    🏋

--- babel.config.js
+++ babel.config.js
@@ -17,7 +17,7 @@ module.exports = {
   presets: ["next/babel", "@babel/preset-typescript"],
   plugins: [
     [
-      // ISSUE: Module path maps are not resolved in emitted code, https://github.com/microsoft/TypeScript/issues/10866
+      // ⌐╦╦═─ ISSUE: Module path maps are not resolved in emitted code, https://github.com/microsoft/TypeScript/issues/10866
       // https://github.com/tleunen/babel-plugin-module-resolver/blob/master/DOCS.md
       // Alternative https://arunmichaeldsouza.com/blog/aliasing-module-paths-in-node-js
       "module-resolver",
